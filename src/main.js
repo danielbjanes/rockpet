7fd99d7bad42919184e54b3ac03aaafad9d58d11
Sean Coyne <stc3245@rit.edu>
2022-02-09 08:26:51 -0500

    Added basic sidebar for interactions with rock

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import './style.css'
 
 import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry';
+import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+import GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.16/+esm';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -17,6 +22,24 @@ scene.add( cube );
 
 camera.position.z = 5;
 
+function showGUI(){
+  const gui = new GUI({width: 500});
+  const rockProperties = {
+    'Rock Name': 'Rocky the Rock',
+    'Rock Weight': 4,
+    'Feed Rock': function() { alert( 'Rock fed' ) },
+    'Walk Your Rock': function() { alert( 'Rock walked' ) },
+    'Textue': 'Texture 1'
+  }
+
+  gui.add( rockProperties, 'Rock Name' );
+  gui.add( rockProperties, 'Rock Weight' );
+  gui.add( rockProperties, 'Feed Rock');
+  gui.add( rockProperties, 'Walk Your Rock')
+  gui.add( rockProperties, 'Texture', [ 'Texture 1', 'Texture 2', 'Texture 3' ] )
+
+}
+
 function animate() {
   requestAnimationFrame( animate );
 
@@ -25,5 +48,5 @@ function animate() {
 
   renderer.render( scene, camera );
 }
-
+showGUI();
 animate();
